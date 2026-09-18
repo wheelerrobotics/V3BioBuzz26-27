a8bdf7c5fd79e0fa0f6b61cc343823394548cdd5
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2026-09-17 21:21:19 -0400

    pose change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bioBuzz/helpers/PoseLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bioBuzz/helpers/PoseLib.java
@@ -20,7 +20,7 @@ public class PoseLib {
     //field poses
     public static Pose startNectarSide = field.of(58,8, 90);
     public static Pose startEmptySide = field.of(58,133.5, 270);
-    public static Pose passUnderCTRL = field.of(68,70.75, 0);
+    public static Pose passUnderCTRL = field.of(71,70.75, 0);
     public static Pose sideFlower = field.of(47.5, 133.5, 90); public static Pose sideFlowerCTRL = field.of(47.5, 124.5, 90);
     public static Pose bottomFlower = field.of(8, 47.5, 180); public static Pose bottomFlowerCTRL = field.of(17, 47.5, 180);
     public static Pose partnerPickup = field.of(31, 133.5, 90); public static Pose partnerPickupCTRL = field.of(35, 113, 90);
